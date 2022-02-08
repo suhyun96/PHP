63c29711d83a50e88311a02abeb211e80d52bc9c
suhyun choi <95855890+suhyun96@users.noreply.github.com>
2022-02-09 00:47:58 +0900

    Create error.php
    에러 확인 
    
    2022-02-09
    에러 확인용

---
+++ Foundation/error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>error php</title>
+
+    <h1>PHP 에러 확인 </h1>
+    
+    <?php
+        $name = "suhyun";
+
+        //따옴표 안 적고 에러 발생 
+        echo "반갑습니다 . $name";
+
+        echo $nickname;
+
+
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
